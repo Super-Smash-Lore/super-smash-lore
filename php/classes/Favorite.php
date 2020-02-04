deac6d46e44b6d6a719d2bb414d2de878cfeb100
Josh Mashke <jmashke@cnm.edu>
2020-02-04 13:09:22 -0700

    created class and state variables for game and favorite

---
+++ php/classes/Favorite.php
@@ -0,0 +1,17 @@
+<?php
+
+class Favorite {
+	/*
+	 * favorite character Id
+	 */
+	private $favoriteCharacterId;
+	/*
+	 * profile Id for favorite
+	 */
+	private $favoriteProfileId;
+	/*
+	 * date favorited
+	 */
+	private $favoriteDate;
+
+}
---
+++ php/classes/Game.php
@@ -0,0 +1,25 @@
+<?php
+
+class Game {
+	/*
+	 * Id for game
+	 */
+	private $gameId;
+	/*
+	 * Character Id for games
+	 */
+	private $gameCharacterId;
+	/*
+	 * pictures of the games
+	 */
+	private $gamePicture;
+	/*
+	 * systems the games can be played on
+	 */
+	private $gameSystem;
+	/*
+	 * URL to buy game
+	 */
+	private $gameUrl;
+
+}
