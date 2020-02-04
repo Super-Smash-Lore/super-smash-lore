410e8f254480cf340e23d6df52869766bf349711
Josh Mashke <jmashke@cnm.edu>
2020-02-04 10:55:19 -0700

    created class and state variables for Character

---
+++ php/classes/Character.php
@@ -0,0 +1,32 @@
+<?php
+
+class Character {
+	/*
+	 * id for character: this is the primary key
+	 */
+	private $characterId;
+	/*
+	 * description of character
+	 */
+	private $characterDescription;
+	/*
+	 * music for character
+	 */
+	private $characterMusicUrl;
+	/*
+	 * picture for character
+	 */
+	private $characterPictureUrl;
+	/*
+	 * quotes from the character
+	 */
+	private $characterQuotes;
+	/*
+	 * date the character was released
+	 */
+	private $characterReleaseDate;
+	/*
+	 * song for the character
+	 */
+	private $characterSong;
+}
