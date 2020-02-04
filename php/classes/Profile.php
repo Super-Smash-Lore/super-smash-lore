a6061eac55d7789411eceab606fab032d63e41e8
Daniel Gonzales <dgonzales371@cnm.edu>
2020-02-04 10:44:32 -0700

    We made the state variables and declared classes

--- php/classes/Profile.php
+++ php/classes/Profile.php
@@ -2,19 +2,31 @@
 
 class Profile{
 /*
- *
+ * id for profile: this is a primary key
  */
-	private
-	
-	private
-
-	private
-
-	private
-
-	private
-
-	private
-
-	private
+	private $profileId;
+/*
+ * activation token for profile
+ */
+	private $activationToken;
+/*
+ * date user joined
+ */
+	private $dateJoined;
+/*
+ *  description of the profile
+ */
+	private $profileDescription;
+/*
+ *  email for the profile
+ */
+	private $profileEmail;
+/*
+ * hash for the profile
+ */
+	private $profileHash;
+/*
+ *  username for the profile
+ */
+	private $profileUserName;
 }
