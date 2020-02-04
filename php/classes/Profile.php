49d3dfae7f333894945846a095f3dbc27ba1b26a
Josh Mashke <jmashke@cnm.edu>
2020-02-04 14:20:24 -0700

    created construct method, accessor method for activationToken, and setter for activationToken

--- php/classes/Profile.php
+++ php/classes/Profile.php
@@ -48,7 +48,7 @@ class Profile{
 	public function __construct($newProfileId, $newActivationToken, $newDateJoined, $newProfileDescription, $newProfileEmail, $newProfileHash, $newProfileUserName = null) {
 		try {
 			$this->setProfileId($newProfileId);
-			$this->setProfileActivationToken($newActivationToken);
+			$this->setActivationToken($newActivationToken);
 			$this->setDateJoined($newDateJoined);
 			$this->setProfileDescription($newProfileDescription);
 			$this->setProfileEmail($newProfileEmail);
@@ -85,5 +85,73 @@ class Profile{
 	 * accessor method for activation token
 	 */
 
-	public function getActivationToken
+	public function getActivationToken() : ?string {
+		return $this->activationToken;
+	}
+
+	/*
+	 * setter for activation token
+	 */
+
+	public function setActivationToken(string $newActivationToken) : void {
+		if($newActivationToken === null) {
+			$this->activationToken = null;
+			return;
+		}
+		$newActivationToken = strtolower(trim($newActivationToken));
+		if(strlen($newActivationToken) !== 32) {
+			throw (new\RangeException("Activation token has to be 32 character"));
+		}
+		$this->activationToken = $newActivationToken;
+	}
+
+	/*
+	 * accessor for dateJoined
+	 */
+
+	public function getDateJoined() {
+
+	}
+
+	/*
+	 * setter for date Joined
+	 */
+
+	public function setDateJoined(){
+
+	}
+
+	/*
+	 * accessor for profile description
+	 */
+	public function getProfileDescription() : ?string{
+		return $this->profileDescription;
+	}
+
+	/*
+	 * setter for profile description
+	 */
+
+	public function setProfileDescription(string $newProfileDescription): void {
+		$newProfileDescription = trim($newProfileDescription);
+		$newProfileDescription = filter_var($newProfileDescription, FILTER_SANITIZE_STRING, FILTER_FLAG_NO_ENCODE_QUOTES);
+		if(empty($newProfileDescription) === true) {
+			throw (new \InvalidArgumentException("description is empty or insecure");
+		}
+		if(strlen($newProfileDescription) > 255) {
+			throw (new \RangeException("Must be less than 255 characters"));
+		}
+		$this->profileDescription = $newProfileDescription;
+	}
+
+	/*
+	 * accessor for profile Email
+	 */
+	public function getProfileEmail() {
+		return $this->profileEmail;
+	}
+
+	/*
+	 * setter for
+	 */
 }
