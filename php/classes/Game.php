4530f790afaf5a4075c29b462e3590f1674d2116
Josh Mashke <jmashke@cnm.edu>
2020-02-05 13:56:23 -0700

    created constructor for Game

--- php/classes/Game.php
+++ php/classes/Game.php
@@ -22,4 +22,34 @@ class Game {
 	 */
 	private $gameUrl;
 
+	/*
+	 * constructor method for Game
+	 * @param $newGameId
+	 * @param $newGameCharacterId
+	 * @param $newGamePicture
+	 * @param $newGameSystem
+	 * @param $newGameUrl
+	 * @throws \InvalidArgumentException if data types are not valid
+	 * @throws \RangeException if data values are too large or too small
+	 * @throws \TypeError if data type violates a data hint
+	 * @throws \Exception if some other exception occurs
+	 */
+	public function __construct($newGameId, $newGameCharacterId, $newGamePicture, $newGameSystem, $newGameUrl) {
+		try {
+			$this->setGameId($newGameId);
+			$this->setGameCharacterId($newGameCharacterId);
+			$this->setGamePicture($newGamePicture);
+			$this->setGameSystem($newGameSystem);
+			$this->setGameUrl($newGameUrl);
+		}
+		catch(\InvalidArgumentException | \RangeException | \Exception | \TypeError $exception) {
+			$exceptionType = get_class($exception);
+			throw (new $exceptionType($exception->getMessage(), 0, $exception));
+		}
+	}
+
+
+	/*
+	 *
+	 */
 }
