15114e3e53a73284d52028fd08763d903a1a0d0e
Daniel Gonzales <dgonzales371@cnm.edu>
2020-02-11 10:27:34 -0700

    I have finished creating Profile getFooByBar methods

--- php/classes/Profile.php
+++ php/classes/Profile.php
@@ -275,4 +275,42 @@ class Profile{
 		}
 		return ($profile);
 }
+/**
+ * get the profile by profile activation token
+ *
+ * @param string $profileActivationToken
+ * @param \PDO object $pdo
+ * @returns Profile|null Profile or null if not found
+ * @throws /\PDOException when mySQL related errors occur
+ * @throws /\TypeError when variables are not the correct data type
+ **/
+	public static function getProfileByProfileActivationToken(\PDO $pdo, string $profileActivationToken) : ?Profile {
+// make sure activation token is in the right format and that it is a string representation of a hexadecimal
+		$profileActivationToken = trim($profileActivationToken);
+		if(ctype_xdigit($profileActivationToken) === false) {
+			throw(new \InvalidArgumentException("profile activation token is empty or in the wrong format"));
+		}
+
+//create query template
+		$query = "SELECT profileId, profileActivationToken, profileDateJoined, profileEmail, profileHash, profileUserName FROM profile WHERE profileActivationToken = :profileActivationToken";
+		$statement = $pdo->prepare($query);
+
+// bind the profile activation token to the placeholder in the template
+		$parameters = ["profileActivationToken" => $profileActivationToken];
+		$statement->execute($parameters);
+
+//grab the Profile from mySQL
+		try {
+			$profile = null;
+			$statement->setFetchMode(\PDO::FETCH_ASSOC);
+			$row = $statement->fetch();
+			if($row !== false) {
+				$profile = new Profile($row["profileId"], $row["profileActivationToken"], $row["profileDateJoined"], $row["profileEmail"], $row["profileHash"], $row["profileUserName"]);
+			}
+		} catch(\Exception $exception) {
+// if the row couldn't be converted, rethrow it
+			throw(new \PDOException($exception->getMessage(), 0, $exception));
+		}
+		return ($profile);
+	}
 }
