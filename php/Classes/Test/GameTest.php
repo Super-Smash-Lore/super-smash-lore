3d73b7007b5906c0766847d85313ebe92aed1553
Josh Mashke <jmashke@cnm.edu>
2020-02-12 16:45:16 -0700

    made insert test valid game

--- php/Classes/Test/GameTest.php
+++ php/Classes/Test/GameTest.php
@@ -1,6 +1,7 @@
 <?php
 namespace SuperSmashLore\SuperSmashLore;
 
+use Ramsey\Uuid\Uuid;
 use SuperSmashLore\SuperSmashLore\Game;
 
 //grab the class under scrutiny
@@ -14,6 +15,11 @@ require_once (dirname(__DIR__, 2) . "/ValidateUuid.php");
  * @author Josh Mashke <jmashke@cnm.edu>
  **/
 class GameTest extends SuperSmashLoreTest {
+	/**
+	 * game Id
+	 * @var Uuid game Id
+	 */
+	
 	/**
 	 * game that a character comes from; This is for foreign kew relations
 	 **/
@@ -41,12 +47,40 @@ class GameTest extends SuperSmashLoreTest {
 	 **/
 	public final function setUp() : void {
 		// create and insert character to own the test game
-		$this->characterId = new Character(generateUuidv4(), null, "charactermusic.com", "characterpucture.com",
-			"regmvfmverfvev", 12/12/12, "itsbritneybitch", 215);
+		$this->characterId = new Character(generateUuidV4(), null, "characterMusic.com", "britneyBitch",
+			"characterPicture.com", "whatever", "right now", 215, "iurgieuni");
 		$this->characterId->insert($this->getPDO());
 	}
 
 	/**
-	 *
+	 *test inserting a valid game and verifying that the actual mySQL data matches
 	 **/
+	public function testInsertValidGame() : void {
+
+		//create a new Game and insert into mySQL
+		$gameId = generateUuidV4();
+		$game = new Game($gameId, $this->characterId->getCharacterId(), $this->VALID_GAME_URL, $this->VALID_GAME_SYSTEM, $this->VALID_PICTURE_URL);
+		$game->insert($this->getPDO());
+
+		//grb the data from mySQL nd enforce the fields match our expectations
+		$pdoGame = Game::getGameByGameId($this->getPDO(), $game->getGameId());
+		$this->assertEquals($pdoGame->getGameId()->toString(), $gameId->toString());
+		$this->assertEquals($pdoGame->getGameCharacterId(), $game->getGameId()->toString());
+		$this->assertEquals($pdoGame->getGameSystem(), $this->VALID_GAME_SYSTEM);
+		$this->assertEquals($pdoGame->getGamePicture(), $this->VALID_PICTURE_URL);
+		$this->assertEquals($pdoGame->getGameUrl(), $this->VALID_GAME_URL);
+	}
+
+	/**
+	 * test inserting, editing, and updating it
+	 **/
+	public function testUpdateValidGame() : void {
+		//count the number of rows and save it for later
+		$numRows = $this->getConnection()->getRowCount("game");
+		//create a new game and insert into mySQL
+		$gameId = generateUuidV4();
+		$game = new Game($gameId, $this->characterId->getCharacterId(), $this->VALID_GAME_URL, $this->VALID_GAME_SYSTEM, $this->VALID_PICTURE_URL);
+		$game->insert($this->getPDO());
+		//create
+	}
 }
