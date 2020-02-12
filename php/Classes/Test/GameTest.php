d0ca6b8305a158f8dc7aceff6165ead3f4f98326
Josh Mashke <jmashke@cnm.edu>
2020-02-12 16:06:16 -0700

    made state variables, and final function

--- php/Classes/Test/GameTest.php
+++ php/Classes/Test/GameTest.php
@@ -19,9 +19,32 @@ class GameTest extends SuperSmashLoreTest {
 	 **/
 	protected $characterId = null;
 	 /**
-	  * valid picture 
+	  * valid picture url
+	  * @var string $VALID_PICTURE_URL
 	  **/
 	protected $VALID_PICTURE_URL;
+
+	/**
+	 *valid game system
+	 * @var string $VALID_GAME_SYSTEM
+	 **/
+	protected $VALID_GAME_SYSTEM;
+
+	/**
+	 * valid game url
+	 * @var string $VALID_GAME_URL
+	 **/
+	protected $VALID_GAME_URL;
+
+	/**
+	 * create dependent objects before running each test
+	 **/
+	public final function setUp() : void {
+		// create and insert character to own the test game
+		$this->characterId = new Character(generateUuidv4(), null, "charactermusic.com", "characterpucture.com",
+			"regmvfmverfvev", 12/12/12, "itsbritneybitch", 215);
+		$this->characterId->insert($this->getPDO());
+	}
 
 	/**
 	 *
