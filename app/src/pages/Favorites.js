7468827f1d9c9d056edff5dd4f1074e6a00d8711
Ryan Torske <rtorske@cnm.edu>
2020-03-10 14:31:21 -0600

    Adjusted layout to have a scrolling section, and profile info is on the bottom.

--- app/src/pages/Favorites.js
+++ app/src/pages/Favorites.js
@@ -1,23 +1,18 @@
 import React from "react";
 import Card from "react-bootstrap/Card";
-import CardColumns from "react-bootstrap/CardColumns";
-import Row from "react-bootstrap/Row";
-import Col from "react-bootstrap/Col";
-import Form from "react-bootstrap/Form";
-import InputGroup from "react-bootstrap/InputGroup";
-import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
-import FormControl from "react-bootstrap/FormControl";
-import Button from "react-bootstrap/Button";
 import CardDeck from "react-bootstrap/CardDeck";
 import Container from 'react-bootstrap/Container';
 import Image from "../img/placeholder.png";
 
+
 export const Favorites = () => {
 	return (
 		<>
-			<main className="my-5">
-				<Container fluid="true" className="container-fluid text-center text-md-center">
-					<h1 className="text-md-left">Your Favorited Characters:</h1>
+			<div className="Title pl-3 pt-4">
+			<h1 className="text-md-left pt-3">Your Favorite Characters:</h1>
+			</div>
+			<main className="Main my-4 align-items-baseline">
+				<Container fluid="true" className="container-fluid text-center text-md-center pre-scrollable col-xl-12">
 					<CardDeck>
 						<Card className="card-body col-xl-2 border-0">
 							<Card.Img variant="top" src={Image}/>
@@ -43,14 +38,20 @@ export const Favorites = () => {
 								<Card.Title>Character Name</Card.Title>
 							</Card.Body>
 						</Card>
-						<div className="Right-modal-upper col-xl-3 border-dark border-left">
-							<div className="row-3 pt-5">
-								<div className="row-3 pt-5">
-									<h2>Profile: Name</h2>
-									<a type="button" href="/about-us" className="btn btn-primary">Profile Settings</a>
-								</div>
-							</div>
-						</div>
+						<Card className="card-body col-xl-2 border-0">
+							<Card.Img variant="top" src={Image} />
+							<Card.Body>
+								<Card.Title>Character Name</Card.Title>
+							</Card.Body>
+						</Card>
+						{/*<div className="Right-modal-upper col-xl-3 border-dark border-left">*/}
+						{/*	<div className="row-3 pt-5">*/}
+						{/*		<div className="row-3 pt-5">*/}
+						{/*			<h2>Profile: Name</h2>*/}
+						{/*			<a type="button" href="/about-us" className="btn btn-primary">Profile Settings</a>*/}
+						{/*		</div>*/}
+						{/*	</div>*/}
+						{/*</div>*/}
 					</CardDeck>
 					<CardDeck>
 						<Card className="card-body col-xl-2 border-0">
@@ -77,12 +78,42 @@ export const Favorites = () => {
 								<Card.Title>Character Name</Card.Title>
 							</Card.Body>
 						</Card>
-						<div className="Right-modal-lower col-xl-3 border-dark border-left">
-						<h1>Save all of your favorite fighters here to see information about them whenever you want.</h1>
-						</div>
+						<Card className="card-body col-xl-2 border-0">
+							<Card.Img variant="top" src={Image} />
+							<Card.Body>
+								<Card.Title>Character Name</Card.Title>
+							</Card.Body>
+						</Card>
+						{/*<div className="Right-modal-lower col-xl-3 border-dark border-left">*/}
+						{/*<h1>Save all of your favorite fighters here to see information about them whenever you want.</h1>*/}
+						{/*</div>*/}
 					</CardDeck>
 				</Container>
 			</main>
+			{/*<div className="container2">*/}
+			{/*	<div className="row2">*/}
+			{/*		<div className="Right-modal-upper col-xl-11 border-dark border-top align-self-center pl-5">*/}
+			{/*			<div className="row-3">*/}
+			{/*				<div className="row-1">*/}
+			{/*					<h2>Profile: Name</h2>*/}
+			{/*					<a type="button" href="/about-us" className="btn btn-primary">Profile Settings</a>*/}
+			{/*					<div className="d-flex">*/}
+			{/*					<h1>Save all of your favorite fighters here to see information about them whenever you want.</h1>*/}
+			{/*					</div>*/}
+			{/*				</div>*/}
+			{/*			</div>*/}
+			{/*		</div>*/}
+			{/*	</div>*/}
+			{/*</div>*/}
+			<div className="d-flex pt-3 mt-5 col-xl-12">
+				<div className="mr-auto p-2 col-xl-4 pl-5">
+					<h2>Profile: Name</h2>
+					<a type="button" href="/about-us" className="btn btn-primary">Profile Settings</a>
+				</div>
+				<div className="p-2 align-self-center col-xl-8">
+					<h1>Save all of your favorite fighters here to see information about them whenever you want.</h1>
+				</div>
+			</div>
 		</>
 	)
 };
