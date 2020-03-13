b7fba7a9d6694a399536e110d0c8ecfc496ec541
Ryan Buchholz <rbuchholz425@gmail.com>
2020-03-13 11:59:48 -0600

    checkpoint.

--- app/src/index.js
+++ app/src/index.js
@@ -2,7 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom'
 import 'bootstrap/dist/css/bootstrap.css';
 import "./index.css";
-import "./index-home.css"
 import {BrowserRouter} from "react-router-dom";
 import {Route, Switch} from "react-router";
 import {FourOhFour} from "./pages/FourOhFour";
--- app/src/pages/Home.js
+++ app/src/pages/Home.js
@@ -1,13 +1,15 @@
 import React from "react"
-import splash from "../img/Smash-Background.png"
+import splash from "../img/Odyssey-Of-Ultimate-Banner-2.png"
 import Card from "react-bootstrap/Card";
 import Container from "react-bootstrap/Container";
 import Logo from "../img/OoD-Logo-v2.png"
+import Button from "react-bootstrap/Button";
 
 export const Home = () => (
 			<div className="home">
-			<h1>Odyssey</h1>
-			<h1>Of Ultimate</h1>
+			<h1 id="title-top">Odyssey</h1>
+				<h1 id="Of">Of</h1>
+			<h1>Ultimate</h1>
 				<div>
 					<p id="developed-by">
 						Developed By:
@@ -15,6 +17,14 @@ export const Home = () => (
 					<p id="developed-by-2">
 						Deep Diving for Nintendo
 					</p>
+				</div>
+				<div>
+					<p id="started">
+						Let's get started!
+					</p>
+					<div id="start-button">
+						<a type="button" href="FighterInfo.js/" className="btn btn-dark btn-lg">Fighters</a>
+					</div>
 				</div>
 			</div>
 	);
