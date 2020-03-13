717ad3f5ffb208b0a2d431d397a35d4a2d1698e4
Ryan Torske <rtorske@cnm.edu>
2020-03-13 12:42:00 -0600

    Finished the favorites section. Functional in mobile forms.

--- app/src/pages/Favorites.js
+++ app/src/pages/Favorites.js
@@ -1,158 +1,168 @@
 import React from "react";
 import Card from "react-bootstrap/Card";
-import CardDeck from "react-bootstrap/CardDeck";
+import CardImg from "react-bootstrap/CardImg";
+import Button from "react-bootstrap/Button";
 import Container from 'react-bootstrap/Container';
-import Image from "../img/placeholder.png";
-import "../styles.css"
-
-
-export const Favorites = () => {
-	return (
-		<>
-			<div className="Title pt-4">
-			<h1 className="text-md-left pt-3">Your Favorite Characters:</h1>
+// import Alert from '/react-bootstrap/react-bootstrap.alert'
+import CardDeck from "react-bootstrap/CardDeck";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
+// import Image from "../img/OoD-Logo-v2.png";
+import Placeholder from "../img/placeholder.png";
+import Navbar from "react-bootstrap/Navbar";
+import Nav from "react-bootstrap/Nav";
+export const Favorites = () => (
+	<body id="favorites-body">
+	<Navbar sticky="top" bg="dark" variant="dark" collapseOnSelect expand="lg">
+		<Navbar.Brand href="/"><img id="Logo-2" className="ml-5"  alt="nav-logo"/></Navbar.Brand>
+		<Navbar.Toggle aria-controls="responsive-navbar-nav" />
+		<Navbar.Collapse id="responsive-navbar-nav">
+			<div className="navbar-collapse flex-row-reverse" id="navbarNavAltMarkup">
+				<Nav.Link className="nav-link nav-item active js-scroll-trigger" href="about">About</Nav.Link>
+				<Nav.Link className="nav-link nav-item active js-scroll-trigger" href="/fighter-selection">Fighters</Nav.Link>
+				<Nav.Link className="nav-link nav-item active js-scroll-trigger" href="#profile">Profile</Nav.Link>
+				<Nav.Link className="nav-link nav-item active js-scroll-trigger" href="#favorites">Favorites</Nav.Link>
+				<Nav.Link className="nav-link nav-item active js-scroll-trigger" href="/sign-in">Sign-In</Nav.Link>
+				<Nav.Link className="nav-link nav-item active js-scroll-trigger" href="#signUp">Sign-Up</Nav.Link>
 			</div>
-			<main className="Main align-items-baseline ml-5">
-				<Container fluid="true" className="pre-scrollable container-fluid text-center text-md-center col-xl-12">
-					<CardDeck>
-						<Card className="card-body col-xl-2 border-0">
-							<Card.Img variant="top" src={Image}/>
-							<Card.Body>
-								<Card.Title>Character Name</Card.Title>
-							</Card.Body>
-						</Card>
-						<Card className="card-body col-xl-2 border-0">
-							<Card.Img variant="top" src={Image} />
-							<Card.Body>
-								<Card.Title>Character Name</Card.Title>
-							</Card.Body>
-						</Card>
-						<Card className="card-body col-xl-2 border-0">
-							<Card.Img variant="top" src={Image} />
-							<Card.Body>
-								<Card.Title>Character Name</Card.Title>
-							</Card.Body>
-						</Card>
-						<Card className="card-body col-xl-2 border-0">
-							<Card.Img variant="top" src={Image} />
-							<Card.Body>
-								<Card.Title>Character Name</Card.Title>
-							</Card.Body>
-						</Card>
-						<Card className="card-body col-xl-2 border-0">
-							<Card.Img variant="top" src={Image} />
-							<Card.Body>
-								<Card.Title>Character Name</Card.Title>
-							</Card.Body>
-						</Card>
-					</CardDeck>
-					<CardDeck>
-					<Card className="card-body col-xl-2 border-0">
-						<Card.Img variant="top" src={Image}/>
-						<Card.Body>
-							<Card.Title>Character Name</Card.Title>
-						</Card.Body>
-					</Card>
-					<Card className="card-body col-xl-2 border-0">
-						<Card.Img variant="top" src={Image} />
-						<Card.Body>
-							<Card.Title>Character Name</Card.Title>
-						</Card.Body>
-					</Card>
-					<Card className="card-body col-xl-2 border-0">
-						<Card.Img variant="top" src={Image} />
-						<Card.Body>
-							<Card.Title>Character Name</Card.Title>
-						</Card.Body>
-					</Card>
-					<Card className="card-body col-xl-2 border-0">
-						<Card.Img variant="top" src={Image} />
-						<Card.Body>
-							<Card.Title>Character Name</Card.Title>
-						</Card.Body>
-					</Card>
-					<Card className="card-body col-xl-2 border-0">
-						<Card.Img variant="top" src={Image} />
-						<Card.Body>
-							<Card.Title>Character Name</Card.Title>
-						</Card.Body>
-					</Card>
-				</CardDeck>
-					<CardDeck>
-						<Card className="card-body col-xl-2 border-0">
-							<Card.Img variant="top" src={Image}/>
-							<Card.Body>
-								<Card.Title>Character Name</Card.Title>
-							</Card.Body>
-						</Card>
-						<Card className="card-body col-xl-2 border-0">
-							<Card.Img variant="top" src={Image} />
-							<Card.Body>
-								<Card.Title>Character Name</Card.Title>
-							</Card.Body>
-						</Card>
-						<Card className="card-body col-xl-2 border-0">
-							<Card.Img variant="top" src={Image} />
-							<Card.Body>
-								<Card.Title>Character Name</Card.Title>
-							</Card.Body>
-						</Card>
-						<Card className="card-body col-xl-2 border-0">
-							<Card.Img variant="top" src={Image} />
-							<Card.Body>
-								<Card.Title>Character Name</Card.Title>
-							</Card.Body>
-						</Card>
-						<Card className="card-body col-xl-2 border-0">
-							<Card.Img variant="top" src={Image} />
-							<Card.Body>
-								<Card.Title>Character Name</Card.Title>
-							</Card.Body>
-						</Card>
-					</CardDeck>
-					<CardDeck>
-						<Card className="card-body col-xl-2 border-0">
-							<Card.Img variant="top" src={Image}/>
-							<Card.Body>
-								<Card.Title>Character Name</Card.Title>
-							</Card.Body>
-						</Card>
-						<Card className="card-body col-xl-2 border-0">
-							<Card.Img variant="top" src={Image} />
-							<Card.Body>
-								<Card.Title>Character Name</Card.Title>
-							</Card.Body>
-						</Card>
-						<Card className="card-body col-xl-2 border-0">
-							<Card.Img variant="top" src={Image} />
-							<Card.Body>
-								<Card.Title>Character Name</Card.Title>
-							</Card.Body>
-						</Card>
-						<Card className="card-body col-xl-2 border-0">
-							<Card.Img variant="top" src={Image} />
-							<Card.Body>
-								<Card.Title>Character Name</Card.Title>
-							</Card.Body>
-						</Card>
-						<Card className="card-body col-xl-2 border-0">
-							<Card.Img variant="top" src={Image} />
-							<Card.Body>
-								<Card.Title>Character Name</Card.Title>
-							</Card.Body>
-						</Card>
-					</CardDeck>
-				</Container>
-			</main>
-			<div className="Profile d-flex pt-3 col-xl-12 border-top border-dark">
-				<div className="mr-auto p-2 col-xl-4 pb-5 pl-5 pt-2">
-					<h2 id="Profile-section">Profile: Name</h2>
-					<a type="button" href="/about-us" className="btn btn-primary">Profile Settings</a>
+		</Navbar.Collapse>
+	</Navbar>
+	<main className="my-5 flex-row">
+		<Container fluid="true" className="container-fluid">
+			<Row>
+				<div className="text-right col-12">
+					<h1 className="" id="character-name">Your Favorites:</h1>
 				</div>
-				<div className="p-2 align-self-cent0er col-xl-8">
-					<h1>Save all of your favorite fighters here to see information about them whenever you want.</h1>
+			</Row>
+		</Container>
+		<Container fluid="true" className="container-fluid" id="whole-row">
+			<Row className="favorite-row">
+				<Col className="profile-area col-12 offset-lg-0 col-xl-3">
+					<Card className="card-body border-0 text-center" id="card-3">
+						<div className="Profile d-flex pt-3 col-xl-12">
+							<div className="mr-auto col-xl-12 pb-5 pt-2">
+								<h2 id="Profile-section">Profile: Name</h2>
+								<a type="button" href="/about-us" className="btn btn-primary">Profile Settings</a>
+							</div>
+						</div>
+						<div className="p-2 align-self-center col-xl-12">
+							<h3>Save all of your favorite fighters here to see information about them whenever you want.</h3>
+						</div>
+					</Card>
+				</Col>
+				<Col className="favorite-panel pre-scrollable col-12 col-xl-9 border-left border-dark pt-2">
+					<Card className="border-0" id="card-4">
+						<Card.Body>
+							<CardDeck>
+							<Card className="card-body col-xl-12 border-0">
+								<Card.Img variant="top" src={Placeholder}/>
+								<Card.Body>
+									<Card.Title>Character Name</Card.Title>
+								</Card.Body>
+							</Card>
+							<Card className="card-body col-xl-12 border-0">
+								<Card.Img variant="top" src={Placeholder} />
+								<Card.Body>
+									<Card.Title>Character Name</Card.Title>
+								</Card.Body>
+							</Card>
+							<Card className="card-body col-xl-12 border-0">
+								<Card.Img variant="top" src={Placeholder} />
+								<Card.Body>
+									<Card.Title>Character Name</Card.Title>
+								</Card.Body>
+							</Card>
+							<Card className="card-body col-xl-12 border-0">
+								<Card.Img variant="top" src={Placeholder} />
+								<Card.Body>
+									<Card.Title>Character Name</Card.Title>
+								</Card.Body>
+							</Card>
+							<Card className="card-body col-xl-12 border-0">
+								<Card.Img variant="top" src={Placeholder} />
+								<Card.Body>
+									<Card.Title>Character Name</Card.Title>
+								</Card.Body>
+							</Card>
+						</CardDeck>
+							<CardDeck>
+								<Card className="card-body col-xl-12 border-0">
+									<Card.Img variant="top" src={Placeholder}/>
+									<Card.Body>
+										<Card.Title>Character Name</Card.Title>
+									</Card.Body>
+								</Card>
+								<Card className="card-body col-xl-12 border-0">
+									<Card.Img variant="top" src={Placeholder} />
+									<Card.Body>
+										<Card.Title>Character Name</Card.Title>
+									</Card.Body>
+								</Card>
+								<Card className="card-body col-xl-12 border-0">
+									<Card.Img variant="top" src={Placeholder} />
+									<Card.Body>
+										<Card.Title>Character Name</Card.Title>
+									</Card.Body>
+								</Card>
+								<Card className="card-body col-xl-12 border-0">
+									<Card.Img variant="top" src={Placeholder} />
+									<Card.Body>
+										<Card.Title>Character Name</Card.Title>
+									</Card.Body>
+								</Card>
+								<Card className="card-body col-xl-12 border-0">
+									<Card.Img variant="top" src={Placeholder} />
+									<Card.Body>
+										<Card.Title>Character Name</Card.Title>
+									</Card.Body>
+								</Card>
+							</CardDeck>
+							<CardDeck>
+								<Card className="card-body col-xl-12 border-0">
+									<Card.Img variant="top" src={Placeholder}/>
+									<Card.Body>
+										<Card.Title>Character Name</Card.Title>
+									</Card.Body>
+								</Card>
+								<Card className="card-body col-xl-12 border-0">
+									<Card.Img variant="top" src={Placeholder} />
+									<Card.Body>
+										<Card.Title>Character Name</Card.Title>
+									</Card.Body>
+								</Card>
+								<Card className="card-body col-xl-12 border-0">
+									<Card.Img variant="top" src={Placeholder} />
+									<Card.Body>
+										<Card.Title>Character Name</Card.Title>
+									</Card.Body>
+								</Card>
+								<Card className="card-body col-xl-12 border-0">
+									<Card.Img variant="top" src={Placeholder} />
+									<Card.Body>
+										<Card.Title>Character Name</Card.Title>
+									</Card.Body>
+								</Card>
+								<Card className="card-body col-xl-12 border-0">
+									<Card.Img variant="top" src={Placeholder} />
+									<Card.Body>
+										<Card.Title>Character Name</Card.Title>
+									</Card.Body>
+								</Card>
+							</CardDeck>
+						</Card.Body>
+					</Card>
+				</Col>
+			</Row>
+		</Container>
+	</main>
+	<div className="position-static">
+		<footer className="page-footer font-small blue bg-primary">
+			<div className="footer-copyright text-center mt-5">
+				<div className="d-flex justify-content-center">
+					<p className="my-2">OdysseyOfUltimate.com 2020</p>
 				</div>
 			</div>
-		</>
-	)
-};
+		</footer>
+	</div>
+	</body>
+);
